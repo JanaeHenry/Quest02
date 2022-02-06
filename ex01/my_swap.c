f3adc198f02b8dc63ac1d7cce7c8e58d375a419c
janae henry <henry_j@qwasar.io>
2022-02-06 02:04:44 +0000

    Updated EX01

--- ex01/my_swap.c
+++ ex01/my_swap.c
@@ -1,3 +1,13 @@
+/* Remember this...
+int a = 5; // an integer, contains value
+int *p; // an integer pointer, contains address
+p = &a; // &a means address of a
+a = *p; // *p means value stored in that address, here 5
+*/
+
+
+
+
 #include <stdio.h>
 
 void my_swap(int* param_1, int* param_2)
